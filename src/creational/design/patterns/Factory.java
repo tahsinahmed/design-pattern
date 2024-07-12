f94b9c7d311d5db3400b0e88138a6cf615a0c6b1
TAHSINahmed <st.ahmed14@gmail.com>
2024-07-12 19:58:28 +0600

    factory pattern

--- src/creational/design/patterns/Factory.java
+++ src/creational/design/patterns/Factory.java
@@ -1,4 +1,55 @@
 package creational.design.patterns;
 
 public class Factory {
+    public static void main(String[] args) {
+        Creator creatorA = new ConcreteCreatorA();
+        Product productA = creatorA.factoryMethod();
+        productA.display();
+
+        Creator creatorB = new ConcreteCreatorB();
+        Product productB = creatorB.factoryMethod();
+        productB.display();
+    }
 }
+
+abstract class Product {
+    public abstract void display();
+}
+
+class ConcreteProductA extends Product{
+
+    @Override
+    public void display() {
+        System.out.println("This is a concrete product A");
+    }
+}
+
+class ConcreteProductB extends Product{
+
+    @Override
+    public void display() {
+        System.out.println("This is a concrete product B");
+    }
+}
+
+
+abstract class Creator {
+    public abstract Product factoryMethod();
+}
+
+class ConcreteCreatorA extends Creator {
+    @Override
+    public Product factoryMethod() {
+        return new ConcreteProductA();
+    }
+}
+
+class ConcreteCreatorB extends Creator {
+
+    @Override
+    public Product factoryMethod() {
+        return new ConcreteProductB();
+    }
+}
+
+
